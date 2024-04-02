94773080bfb233bc6dd88c58eafaf7a1fbb40d59
Nikita <mni2004@mail.ru>
2024-04-02 15:08:53 +0300

    Initial commit

---
+++ DragoEchoBot/src/main/java/io/proj3ct/DragoEchoBot/DragoEchoBotApplication.java
@@ -0,0 +1,13 @@
+package io.proj3ct.DragoEchoBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragoEchoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragoEchoBotApplication.class, args);
+	}
+
+}
---
+++ DragoEchoBot/src/test/java/io/proj3ct/DragoEchoBot/DragoEchoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.proj3ct.DragoEchoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragoEchoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
